c091bb2632d62b565012f03af6245ded0aacd6de
summerxzp_hp <505628905@qq.com>
2026-02-11 18:03:39 +0800

    Remove package folder from git tracking

--- package/hook/hook-PyQt6.py
+++
@@ -1,7 +0,0 @@
-from PyInstaller.utils.hooks import collect_all
-
-# 不调用 QtLibraryInfo，直接收集全部 PyQt6 资源
-datas, binaries, hiddenimports = collect_all("PyQt6")
-
-# 明确补充 sip
-hiddenimports += ["PyQt6.sip"]
